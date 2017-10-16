b4611153f3d66e7e7860e3177c19a29b0aaa1a59
mikand13 <mikkelsen.anders@gmail.com>
2017-10-16 13:34:57 +0200

    updated version

--- src/main/java/com/nannoq/tools/cluster/services/package-info.java
+++ src/main/java/com/nannoq/tools/cluster/services/package-info.java
@@ -1,7 +1,7 @@
 /**
  * Created by anders on 28/12/2016.
  */
-@ModuleGen(name = "NannoqHeartbeatService", groupPackage="com.nannoq.tools.cluster.services")
+@ModuleGen(name = "nannoqHeartbeatService", groupPackage="com.nannoq.tools.cluster.services")
 package com.nannoq.tools.cluster.services;
 
 import io.vertx.codegen.annotations.ModuleGen;
